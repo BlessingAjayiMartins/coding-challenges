43b80782b08a0b1c1789692903f54e8a74380f2b
blessing martins <blessmartins@gmail.com>
2019-05-04 19:57:59 -0400

    Reverse string

---
+++ kata8/reversedString.js
@@ -0,0 +1,13 @@
+function solution(str){
+  // create a variable 'result' to hold result
+//   iterate through string in reverse
+//   for each iteration save the output to result
+// return result
+
+  var result = '';
+  for (var i = str.length; i > -1; i--){
+    result += str[i]
+}
+return result
+}
+// Blessing
