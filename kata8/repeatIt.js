d485b17eb2d663ffb59e10ccb7531e7f7ae6b7ad
blessing martins <blessmartins@gmail.com>
2019-05-05 19:51:08 -0400

    Function should return string  n times

---
+++ kata8/repeatIt.js
@@ -0,0 +1,14 @@
+// The function should return a string that repeats the input string 'n' number of times
+// if anything other than a string is not passed; return "Not a string"
+
+var repeatIt = function(str, n) {
+  var result = '';
+    for (i = 0; i < n; i++){
+      if (typeof str === 'string'){
+        result += str
+      }
+      else 
+       return 'Not a string'
+    }
+    return result
+  }
