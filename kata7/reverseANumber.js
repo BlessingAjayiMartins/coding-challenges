28792c3f13b0766af255e8629a25bab00bba712f
blessing martins <blessmartins@gmail.com>
2019-05-09 16:17:12 -0400

    321 to 123 || -321 to -123

--- kata7/reverseANumber.js
+++ kata7/reverseANumber.js
@@ -1,13 +1,21 @@
+// function reverseNumber(n) {
+//   var x = n.toString().split('').reverse().join('')*1
+//   const greaterThanZero = num => {return num > 0}
+//   var y = n.toString().split('').filter(greaterThanZero)
+//   var z = y.reverse().join('')
+//   if (n > 0){
+//     return x
+//   }
+//   else 
+//    return -z
+// }
+
+//        OR
+
 function reverseNumber(n) {
-  var x = n.toString().split('').reverse().join('')*1
-  const greaterThanZero = num => {return num > 0}
-  var y = n.toString().split('').filter(greaterThanZero)
-  var z = y.reverse().join('')
-  if (n > 0){
-    return x
-  }
-  else 
-   return -z
+  var x = Math.abs(n).toString().split('').reverse().join('')*1
+  const isNegative = n < 0
+  return isNegative? -x : x
 }
 console.log(reverseNumber(321))   // should return 123
 console.log(reverseNumber(-321))  // should return -123
