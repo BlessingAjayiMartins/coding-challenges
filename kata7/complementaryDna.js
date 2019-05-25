8741603ab268e84aeba688a66818b0accec035b5
blessing martins <blessmartins@gmail.com>
2019-05-25 09:41:55 -0400

    Complementary DNA completed

---
+++ kata7/complementaryDna.js
@@ -0,0 +1,15 @@
+function DNAStrand(dna){
+  let result = '';
+  for (i = 0; i < dna.length; i++) {
+    if (dna[i] === 'A') {
+      result += 'T'
+    } else if (dna[i] === 'T') {
+        result += 'A'
+      } else if (dna[i] === 'G') {
+          result += 'C'
+        } else if (dna[i] === 'C') {
+            result += 'G'
+          }
+  }
+  return result
+}
