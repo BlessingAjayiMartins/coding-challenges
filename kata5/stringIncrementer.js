16e98852f0b9cedfb88a75112366f213101af2a2
blessing martins <blessmartins@gmail.com>
2019-09-20 13:12:53 -0400

    increment string to create a new string. if string ends with number increment by 1. if string does not end with number, 1 should be appended to string. if number has leading zeros, the amount of digits should be considered.

--- kata5/stringIncrementer.js
+++ kata5/stringIncrementer.js
@@ -1,31 +1,30 @@
 const incrementString = (string) => {
   const regex = /[0-9]/g
   const found = string.match(regex)
-  let endOfFound
   let result
   let join
   if (!found) {
     result = string + '1'
   } else {
-    endOfFound = found[found.length -1]*1
-    if(!endOfFound && endOfFound === 0) {
-      const num = endOfFound + 1
-      result = string.slice(0, -1) + num.toString()
-    } else if (endOfFound < 9) {
-      const num = endOfFound + 1
-      result = string.slice(0, -1) + num.toString()
-    } else {
-      join = found.join('')
-      const newJoin = join * 1 + (1)
-      result = string.slice(0, -join.length) + newJoin.toString()
+    join = found.join('') * 1 + (1)
+    join = join.toString().split('')
+    const arr = Array(found.length).fill(0)
+    for (let i = 0; i < join.length; i++) {
+      arr.push(join[i])
+      if (arr[0] === 0)
+      arr.shift()
     }
-  } 
+    result = string.slice(0, -found.length) + arr.join('')
+    result = result.toString()
+  }
   return result
 }
-
 console.log(incrementString("foobar000")) // "foobar001");
 console.log(incrementString("foo")) //, "foo1");
 console.log(incrementString("foobar001")) //, "foobar002");
 console.log(incrementString("foobar99")) //, "foobar100");
 console.log(incrementString("foobar099")) //, "foobar100");
 console.log(incrementString("")) //, "1");
+console.log(incrementString("foobar00999"))// foobar01000
+console.log(incrementString("foobar009")) // foobar010
+console.log(incrementString("foobar999")) // foobar1000
